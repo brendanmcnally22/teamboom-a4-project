4aa024dd44dbd9124eed22691212a9bc3b0ac77e
brendanmcnally22 <brendanmcnally295@gmail.com>
2025-03-13 11:50:03 -0400

    Add Project - Add Project to Github

---
+++ teamboom-a4-project/teamboom-a4-project/Game.cs
@@ -0,0 +1,23 @@
+﻿// Include the namespaces (code libraries) you need below.
+using System;
+using System.Numerics;
+
+// The namespace your code is in.
+namespace MohawkGame2D;
+
+// Add Project
+public class Game
+{
+ 
+    public void Setup()
+    {
+
+    }
+
+    
+    public void Update()
+    {
+
+    }
+}
+

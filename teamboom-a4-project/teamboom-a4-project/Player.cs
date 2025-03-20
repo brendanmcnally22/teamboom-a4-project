02b310f5a8343ce859b4ba7c9996d33fbb6c6161
brendanmcnally22 <brendanmcnally295@gmail.com>
2025-03-20 14:55:25 -0400

    Fix thing

---
+++ teamboom-a4-project/teamboom-a4-project/Player.cs
@@ -0,0 +1,13 @@
+﻿using System.Numerics;
+using System;
+
+namespace MohawkGame2D
+{
+    public class Player
+    {
+        public void Render(float x, float y)
+        {
+            //add asset loading here
+        }
+    }
+}

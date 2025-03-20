118af2f3cc9774f8fadd30fef5ae77025721fd6e
EphiRobinsonMohawk <000969115@mohawkcollege.ca>
2025-03-20 10:12:20 -0400

    add player class

---
+++ teamboom-a4-project/teamboom-a4-project/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MohawkGame2D;
+
+    public class Player
+    {
+
+
+
+        public void Render(float x, float y)
+    {
+        // Put asset loading here
+    }
+
+    }
+
+
